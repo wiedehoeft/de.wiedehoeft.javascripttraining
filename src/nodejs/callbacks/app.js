739b9537d8e86d9ea86e3124896ceb0a205db920
Christian <wiedehoeft.christian@gmail.com>
2019-11-27 08:23:08 +0100

    Added callback examples

---
+++ src/nodejs/callbacks/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const numbers = [1, 2, 3, 4, 5];
+const squares = numbers.map(n => n ** 2);
+
+console.log(squares);
+
+const words = ['the', 'quick', 'brown', 'fox'];
+const wordLenghts = words.map(word => ({
+    word: word, length: word.length
+}));
+
+console.log(wordLenghts);
---
+++ src/nodejs/callbacks/callback.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const add = (left, right) => {
+    return left + right;
+};
+
+const sum = add(23, 42);
+
+console.log(sum);
+
+
+/* Now as callback */
+const add2 = (left, right, callback) => {
+    const sum = left + right;
+
+    setTimeout(() => {
+        callback(null, sum); // with error handling
+    }, 1 * 1000);
+};
+
+console.log('###1');
+
+
+add2(10, 20, (err, sum) => {
+    console.log(sum);
+});
+
+console.log('###2');
