799a08a7c3195beb43875ced3d7c648e782cafec
Christian <wiedehoeft.christian@gmail.com>
2019-11-28 09:37:44 +0100

    added async test

--- src/nodejs/testing/lib/calculator.js
+++ src/nodejs/testing/lib/calculator.js
@@ -10,4 +10,20 @@ calculator.add = (...numbers) => {
   return numbers.reduce((sum, number) => sum + number);
 };
 
+calculator.addAsync = (callback, ...numbers) => {
+  if (!callback) {
+    throw new Error('Callback is missing');
+  }
+
+  if (numbers.filter(number => !isNaN(number)).length === 0) {
+    throw new Error('Numbers are missing');
+  }
+
+  const sum = numbers.reduce((intermediateSum, number) => intermediateSum + number);
+
+  setTimeout(() => {
+    callback(null, sum);
+  }, 1 * 1000);
+};
+
 module.exports = calculator;
--- src/nodejs/testing/test/calculatorTests.js
+++ src/nodejs/testing/test/calculatorTests.js
@@ -1,31 +1,50 @@
+/* eslint-disable padded-blocks */
 'use strict';
 
 const calculator = require('../lib/calculator');
 const expect = require('expect');
 
 describe('calculator', () => {
-  it('Should return the sum of the given numbers.', () => {
-    // Act
-    const result = calculator.add(1, 2);
 
-    // Assert
-    expect(result).toEqual(3);
+  describe('sync', () => {
+
+    it('Should return the sum of the given numbers.', done => {
+      // Act
+      const result = calculator.add(1, 2);
+
+      // Assert
+      expect(result).toEqual(3);
+      done();
+    });
+
+    it('Should raise an error when charachter is passed.', done => {
+      // Act
+      const result = () => calculator.add('a');
+
+      // Assert
+      expect(result).toThrowError('Numbers are missing');
+      done();
+    });
+
+    it('Should raise an error when no number is passed.', done => {
+      // Act
+      const result = () => calculator.add();
+
+      // Assert
+      expect(result).toThrowError('Numbers are missing');
+      done();
+    });
   });
 
-  it('Should raise an error when charachter is passed.', () => {
-    // Act
-    const result = () => calculator.add('a');
+  describe('async', () => {
 
-    // Assert
-    expect(result).toThrowError('Numbers are missing');
+    it('Should return the sum of the given numbers.', done => {
+      // Act
+      calculator.addAsync((err, sum) => {
+        expect(err).toBeNull();
+        expect(sum).toEqual(3);
+        done();
+      }, 1, 2);
+    });
   });
-
-  it('Should raise an error when no number is passed.', () => {
-    // Act
-    const result = () => calculator.add();
-
-    // Assert
-    expect(result).toThrowError('Numbers are missing');
-  });
-
 });
